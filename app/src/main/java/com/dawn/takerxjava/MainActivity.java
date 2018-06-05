c6f38899e87655ebd2b276b991ad3136b5e0ff77
wangxiongtao <1422559134@qq.com>
2018-06-05 17:38:17 +0800

    提交

---
+++ app/src/main/java/com/dawn/takerxjava/MainActivity.java
@@ -0,0 +1,47 @@
+package com.dawn.takerxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dawn.trx.LogUtil;
+import com.dawn.trx.observable.IEmitter;
+import com.dawn.trx.observable.ObservableCreate;
+import com.dawn.trx.observable.RealObservable;
+import com.dawn.trx.observer.Observer;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        go();
+
+    }
+
+    public void go(){
+        new RealObservable(new ObservableCreate() {
+            @Override
+            public void fun(IEmitter emitter) {
+                emitter.onNext("dsadsadsa");
+                LogUtil.i("==able===thread==>"+Thread.currentThread().getName());
+
+            }
+        }).subscribeOn().observableOn().subscribe(new Observer() {
+            @Override
+            public void onNext(String s) {
+                LogUtil.i("==onNext======>"+s);
+                LogUtil.i("==onNext===thread==>"+Thread.currentThread().getName());
+
+
+            }
+
+            @Override
+            public void onComplete(String s) {
+
+            }
+        });
+
+    }
+}
